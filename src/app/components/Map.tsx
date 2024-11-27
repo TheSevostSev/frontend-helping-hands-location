11a66cf0b648e98f76bab5a4417da7057768f20b
vsevolod <sevostsev@gmail.com>
2024-11-27 18:45:31 +0100

    feat: map is displayed

---
+++ src/app/components/Map.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+const Map: React.FC = () => {
+  const [location, setLocation] = useState<{
+    latitude: number;
+    longitude: number;
+  } | null>(null);
+  const [hasMounted, setHasMounted] = useState(false);
+
+  const defaultLocation = { latitude: 40.4168, longitude: -3.7038 };
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (hasMounted && navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLocation({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+        },
+        (err) => {
+          console.log("Failed to retrieve location: " + err.message);
+          setLocation(defaultLocation);
+        }
+      );
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+      setLocation(defaultLocation);
+    }
+  }, [hasMounted]);
+
+  useEffect(() => {
+    if (hasMounted && location) {
+      const mapContainer = document.getElementById("map");
+      if (mapContainer) {
+        const map = L.map(mapContainer, {
+          center: [location.latitude, location.longitude],
+          zoom: 13,
+        });
+
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(
+          map
+        );
+
+        return () => {
+          map.remove();
+        };
+      }
+    }
+  }, [location, hasMounted]);
+
+  if (!hasMounted) {
+    return null;
+  }
+
+  return <div id="map" style={{ width: "100%", height: "100vh" }} />;
+};
+
+export default Map;
