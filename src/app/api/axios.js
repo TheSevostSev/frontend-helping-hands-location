6c3ee0289c1d6a1099e32a2baaa7fee3788b744a
vsevolod <sevostsev@gmail.com>
2024-12-17 00:30:05 +0100

    fix: correctly set Public backend url

--- src/app/api/axios.js
+++ src/app/api/axios.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 import useTokenStore from "@/stores/useTokenStore";
-import { loadEnvConfig } from "@next/env";
-
-const projectDir = process.cwd();
-loadEnvConfig(projectDir);
 
 export const backendURL = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
 });
+
+console.log("Backend URL:", process.env.NEXT_PUBLIC_BACKEND_URL);
 
 backendURL.interceptors.request.use(
   (config) => {
